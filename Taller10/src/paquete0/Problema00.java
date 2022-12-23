1b3b23568f7cd08c4e708676f91d8bc0d35b9a90
santiagoCH14 <santych.4.2@gmail.com>
2022-12-23 11:06:26 -0500

    Programacion

--- Taller10/src/paquete0/Problema00.java
+++ Taller10/src/paquete0/Problema00.java
@@ -10,25 +10,26 @@ package paquete0;
  * @author reroes
  */
 public class Problema00 {
+
     public static void main(String[] args) {
-        double[][] notas = {{9, 10, 7, 8}, 
-                            {10, 5, 7, 9}, 
-                            {7, 9.4, 10, 9}};
+        double[][] notas = {{9, 10, 7, 8},
+        {10, 5, 7, 9},
+        {7, 9.4, 10, 9}};
         String[] nombresEstudiante = {"Jose A", "Elena J", "Paúl L"};
         double[] sumaNotas = new double[3];
-                            
+
         double suma = 0;
         String reporte = "";
-        
-        for (int i = 0; i < notas.length; i++) { 
-            
-            for (int j = 0; j < notas[i].length; j++) { 
+
+        for (int i = 0; i < notas.length; i++) {
+
+            for (int j = 0; j < notas[i].length; j++) {
                 suma = suma + notas[i][j];
-                     
+
             }
             sumaNotas[i] = suma; // 
         }
-        
+
         // Presentación de resultados
         for (int i = 0; i < nombresEstudiante.length; i++) {
             reporte = String.format("%sEstudiante %s tiene una sumataria "
@@ -37,8 +38,8 @@ public class Problema00 {
                     nombresEstudiante[i],
                     sumaNotas[i]);
         }
-        
+
         System.out.printf("%s\n", reporte);
-        
+
     }
 }
--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -5,10 +5,43 @@
  */
 package paquete2;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 /**
  *
  * @author reroes
  */
 public class Problema02 {
-    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        int[][] ventas = new int[2][5];
+        String[] vendedores = {"Jessica Cole", "Robert Wallace"};
+        int suma = 0;
+
+        for (int i = 0; i < vendedores.length; i++) {
+            for (int j = 0; j < ventas[i].length; j++) {
+                System.out.println("Ingrese la cantidad de ventas hechas en el dia: ");
+                ventas[i][j] = entrada.nextInt();
+
+                suma = suma + ventas[i][j];
+
+            }
+
+        }
+        for (int i = 0; i < vendedores.length; i++) {
+            System.out.printf("Vendedor(a) %s\n", vendedores[i]);
+
+        }
+        System.out.printf("En total se realizaro una cantidad de: %d en ventas.\n", suma);
+
+    }
+
 }
